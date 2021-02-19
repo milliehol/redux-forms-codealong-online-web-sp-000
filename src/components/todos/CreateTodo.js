2401b981c238e5a0b8333e31534167ea64a517bb
Carla Holcomb <carla.programmer@gmail.com>
2021-02-19 03:36:06 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
- 
+
 class CreateTodo extends Component {
   state = {
     text: ''
   };
- 
+
   handleChange = event => {
     this.setState({
       text: event.target.value
     });
   };
- 
+
   handleSubmit = event => {
     event.preventDefault();
     this.props.dispatch({ type: 'ADD_TODO', payload: this.state });
   };
- 
+
   render() {
     return (
       <div>
@@ -35,5 +35,5 @@ class CreateTodo extends Component {
     );
   }
 }
- 
+
 export default connect()(CreateTodo);
